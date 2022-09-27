7ac841b2baecc46b9cb77a97d840dc6f39d90290
kmruban <kyleruban1@outlook.com>
2022-09-27 16:07:39 -0400

    routing

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -12,7 +12,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post("https://mlbstatsapi.us-east-1.elasticbeanstalk.com/User/login", {
+      const { data } = await axios.post("https://localhost:5001/User/login", {
         username,
         password,
       });
